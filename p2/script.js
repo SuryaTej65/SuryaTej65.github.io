6dce9c885e29f183ab84adfb456078047c9db193
SuryaTej65 <se22ucse065@mahindrauniversity.edu.in>
2025-06-06 18:22:19 +0530

    a6 done

--- p2/script.js
+++ p2/script.js
@@ -1,6 +1,6 @@
 const showLogin = () => {
     let str=`
-    <div class='App-Container'>
+    <div class="App-Container">
     <h1>Login Form</h1>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
@@ -21,4 +21,10 @@ const showRegister = () => {
     <p><button onclick='showLogin()'>Already have Acc</button></p>
     `
     root.innerHTML = str
+}
+
+const showHome() => {
+    let str=`
+    <h1> Welcome You just accessed the website </h1>
+    `
 }
